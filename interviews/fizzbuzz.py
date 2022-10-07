e19cb7df8db3dbcd0b8b73ac0dcad9995eaf2cf7
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-07 12:01:36 -0700

    solved problem

--- interviews/fizzbuzz.py
+++ interviews/fizzbuzz.py
@@ -1,15 +1,33 @@
-def fizzbuzz(number):
-    current_number = 1
-    answer = []
+# fizzbuzz is a function which returns the string representation of numbers from 1 to n.
 
-    for current_number in range(1, number + 1):
-        if current_number % 3 == 0:
-            answer.append("Fizz")
-        elif current_number % 5 == 0:
-            answer.append("Buzz")
-        elif current_number % 3 == 0 and current_number % 5 == 0:
-            answer.append("FizzBuzz")
+# But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+def fizzbuzz(n):
+    list_to_return = []
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            list_to_return.append("FizzBuzz")
+        elif i % 3 == 0:
+            list_to_return.append("Fizz")
+        elif i % 5 == 0:
+            list_to_return.append("Buzz")
         else:
-            answer.append(f"{current_number}")
+            list_to_return.append(str(i))
+    return list_to_return
+
+
+# def fizzbuzz(number):
+#     current_number = 1
+#     answer = []
+
+#     for current_number in range(1, number + 1):
+#         if current_number % 3 == 0:
+#             answer.append("Fizz")
+#         elif current_number % 5 == 0:
+#             answer.append("Buzz")
+#         elif current_number % 3 == 0 and current_number % 5 == 0:
+#             answer.append("FizzBuzz")
+#         else:
+#             answer.append(f"{current_number}")
     
-    return answer
+#     return answer
